fbcae5922c33ba26295d6110aac8b40353166959
FK20100010 <154255559+FK20100010@users.noreply.github.com>
2024-10-23 20:27:52 +0800

    Update VIKINGYFY.sh

--- VIKINGYFY.sh
+++ VIKINGYFY.sh
@@ -1,6 +1,6 @@
 git clone -b main --single-branch https://github.com/VIKINGYFY/immortalwrt.git openwrt
 [ -e files ] && mv files openwrt/files
-chmod 755 openwrt/files/*/*/*
+chmod 775 openwrt/files/*/*/*
 cd openwrt
 
 #sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
