e3ec196ab336da211445659f058ce874ed2026e3
FK20100010 <154255559+FK20100010@users.noreply.github.com>
2024-10-24 19:14:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,43 +1,8 @@
 #!/usr/bin/env bash
 
-source /etc/profile
-BASE_PATH=$(cd $(dirname $0) && pwd)
-
-Dev=$1
-Build_Mod=$2
-
-if [[ ! -f $BASE_PATH/deconfig/$Dev.config ]]; then
-    echo "config not fond"
-    exit 0
-fi
-
-if [[ ! -f $BASE_PATH/compilecfg/$Dev.ini ]]; then
-    echo "ini not fond"
-    exit 0
-fi
-
-read_ini_by_key() {
-    local key=$1
-    cat $BASE_PATH/compilecfg/$Dev.ini | awk -F"=" '/^'$key'/ {print $2}'
-}
-
-REPO_URL=$(read_ini_by_key "REPO_URL")
-REPO_BRANCH=$(read_ini_by_key "REPO_BRANCH")
-BUILD_DIR=$(read_ini_by_key "BUILD_DIR")
-if [[ -z $REPO_BRANCH ]]; then
-    REPO_BRANCH="main"
-fi
-
-$BASE_PATH/update.sh $REPO_URL $REPO_BRANCH $BASE_PATH/$BUILD_DIR
-
-\cp -f $BASE_PATH/deconfig/$Dev.config $BASE_PATH/$BUILD_DIR/.config
-
-cd $BASE_PATH/$BUILD_DIR
-make defconfig
-
-if [[ $Build_Mod == "debug" ]]; then
-    exit 0
-fi
-
-make download -j$(nproc)
-make -j$(nproc) || make -j1 || make -j1 V=s
+git clone https://github.com/ZqinKing/wrt_relese.git
+mv vikingyfy.jdc wrt_relese/deconfig/jdc_ax1800pro-ax6600_libwrt.config
+mv vikingyfy.jdc wrt_relese/deconfig/jdc_ax1800pro-ax6600_immwrt.config
+mv vikingyfy.jdc wrt_relese/deconfig/jdc_ax1800pro-ax6600_libwrt.config
+mv files/etc/uci-defaults/99-default-settings wrt_relese/patches/992_set-wifi-uci.sh
+ls | grep -v wrt_relese | xargs rm -rf | mv wrt_relese/* ./
