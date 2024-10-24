75fbd7a69daea56edfa3b4780c491802140ba8d3
FK20100010 <154255559+FK20100010@users.noreply.github.com>
2024-10-24 20:29:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ cp vikingyfy.jdc wrt_relese/deconfig/jdc_ax1800pro-ax6600_libwrt.config
 cp vikingyfy.jdc wrt_relese/deconfig/jdc_ax1800pro-ax6600_immwrt.config
 cp vikingyfy.jdc wrt_relese/deconfig/jdc_ax1800pro-ax6600_libwrt.config
 cp files/etc/uci-defaults/99-default-settings wrt_relese/patches/992_set-wifi-uci.sh
-ls | grep -v wrt_relese | xargs rm -rf 
-mv wrt_relese/* ./ | rm -rf wrt_relese
+ls | grep -v wrt_relese | xargs rm -rf && mv wrt_relese/* ./ && rm -rf wrt_relese
